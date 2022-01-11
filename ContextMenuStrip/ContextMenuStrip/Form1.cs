3a216db2a18c5e51d95f39e8ca87aaa7c7acdfdf
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-01-11 10:56:30 +0100

    Add files via upload

---
+++ ContextMenuStrip/ContextMenuStrip/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContextMenuStrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lijevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (txtLijevi.Text == String.Empty)
+            {
+                MessageBox.Show("Upiši tekst!", "Nema teksta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                txtLijevi.Text = txtDesni.Text;
+                txtLijevi.Clear();
+            }
+        }
+
+        private void desnoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (txtDesni.Text == String.Empty)
+            {
+                MessageBox.Show("Upiši tekst!", "Nema teksta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                txtDesni.Text = txtLijevi.Text;
+                txtDesni.Clear();
+            }
+        }
+    }
+}
